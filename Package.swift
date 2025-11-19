75ecbb53ca968797ee504f12ab4831980f7a4ece
blacktop <blacktop@users.noreply.github.com>
2025-11-19 16:46:02 -0700

    chore: back to Swift 6 baby!

--- Package.swift
+++ Package.swift
@@ -1,30 +1,30 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.2
 import PackageDescription
 
 let package = Package(
-    name: "pdf",
-    platforms: [
-        .macOS(.v13)
-    ],
-    products: [
-        .executable(name: "pdf", targets: ["pdf"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.6.2")
-    ],
-    targets: [
-        .executableTarget(
-            name: "pdf",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser")
-            ],
-            linkerSettings: [
-                .linkedFramework("PDFKit")
-            ]
-        ),
-        .testTarget(
-            name: "pdfTests",
-            dependencies: ["pdf"]
-        )
-    ]
+  name: "pdf",
+  platforms: [
+    .macOS(.v13)
+  ],
+  products: [
+    .executable(name: "pdf", targets: ["pdf"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.6.2")
+  ],
+  targets: [
+    .executableTarget(
+      name: "pdf",
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser")
+      ],
+      linkerSettings: [
+        .linkedFramework("PDFKit")
+      ]
+    ),
+    .testTarget(
+      name: "pdfTests",
+      dependencies: ["pdf"]
+    ),
+  ]
 )
